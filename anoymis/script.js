7c477bbc2503da0ae247ad80e1ac61f2ce149ee0
Sharan <142856354+Sharandeveloper@users.noreply.github.com>
2023-12-07 16:57:22 +0530

    Add files via upload

---
+++ anoymis/script.js
@@ -0,0 +1,22 @@
+ /*let  app =  function  (){
+    console.log('hey!')
+
+} 
+app(); */
+
+
+settimeout(() => {
+    swal({
+        position: 'top-end',
+        icon: 'success',
+        title: 'hello world',
+        showconfirmation: true,
+
+
+
+    })
+}, 5000); 
+
+
+
+
